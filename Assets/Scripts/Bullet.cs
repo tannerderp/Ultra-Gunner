f2476d856002c36181f23952f2d9afd152f1de3e
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2019-07-25 11:01:09 -0600

    bullets despawn off screen, faster walk cycle

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject); //despawn bullet when it's off screen. That way enemies don't die before you can see them.
+    }
+}
