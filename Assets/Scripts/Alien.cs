822bc8eccdbf2fff687d31f3781eb9ae6e897e43
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2019-07-26 15:06:03 -0600

    alien enemies
    all they can do right now is die. they don't even have a sprite yet

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    [SerializeField] int health;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "bullet(Clone)") //take damage when hit by player bullet
+        {
+            health--;
+            if(health < 1)
+            {
+                Destroy(gameObject);
+            }
+            Destroy(collision.gameObject);
+        }
+    }
+}
