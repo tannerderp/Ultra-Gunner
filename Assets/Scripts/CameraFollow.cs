3a81d5cb163afd43b695242d7d1ded2312bbb12d
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2019-08-15 12:34:16 -0600

    camera follows you, larger level

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform player; //transform component of the player
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, 0, -10);
+    }
+}
