905e9122bb46fb5d0b1d75e2859f40fea3b8f97f
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2019-09-24 17:26:26 -0600

    camera in level two and anotha part of level 2
    your mom

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Transform player; //transform component of the player
+    [SerializeField] bool followVertical = false; //whether the camera follows the player vertically or not
+
+    private float cameraY = 0;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x, 0, -10);
+        if (followVertical == true && player.position.y>0)
+        {
+            cameraY = player.position.y;
+        }
+        else
+        {
+            cameraY = 0;
+        }
+        transform.position = new Vector3(player.position.x, cameraY, -10);
     }
 }
