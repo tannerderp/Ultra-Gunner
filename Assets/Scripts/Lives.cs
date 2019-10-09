69e54cfb45f2b1c80868d8ca92255250355ecf51
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2019-10-09 16:17:56 -0600

    lives counter
    still need to add dying, I'll do that later today probably

---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Lives : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+
+    [SerializeField] private int lives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = lives.ToString();
+    }
+}
