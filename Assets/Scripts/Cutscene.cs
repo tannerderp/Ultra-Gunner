7a01cb54b7b8ba437e5b0b595df30c9cc48d1f02
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2019-10-20 14:54:07 -0600

    implement cutscene
    yeetus

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Cutscene : MonoBehaviour
+{
+    public double time;
+    public double currentTime;
+    // Use this for initialization
+    void Start()
+    {
+        time = gameObject.GetComponent<VideoPlayer>().clip.length;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = gameObject.GetComponent<VideoPlayer>().time;
+        if (currentTime > time-0.7)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
