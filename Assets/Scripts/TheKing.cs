7f96aa6931e94c80a9f52ed2880c0159cd1ddfde
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2019-10-29 16:22:08 -0600

    finish final boss
    welp no time for music i guess

--- Assets/Scripts/TheKing.cs
+++ Assets/Scripts/TheKing.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TheKing : MonoBehaviour
 {
     [SerializeField] int health = 100;
+    [SerializeField] float moveSpeed = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,7 @@ public class TheKing : MonoBehaviour
         {
             FindObjectOfType<LevelExit>().LoadTheEnd();
         }
+        transform.position = new Vector2(transform.position.x + moveSpeed, transform.position.y);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,17 +31,25 @@ public class TheKing : MonoBehaviour
             Destroy(collision.gameObject);
             if(health < 50)
             {
-                GetComponentInChildren<AimingAlienArm>().timeBetweenBullet = 75f;
+                GetComponentInChildren<AimingAlienArm>().timeBetweenBullet = 50f;
             }
             if(health < 25)
             {
-                GetComponentInChildren<AimingAlienArm>().timeBetweenBullet = 50f;
+                GetComponentInChildren<AimingAlienArm>().timeBetweenBullet = 25f;
             }
         }
         if(health < 1)
         {
             Destroy(GetComponentInChildren<AimingAlienArm>());
-            gameObject.AddComponent<Rigidbody2D>();
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+        if (collision.gameObject.tag == "King Flipper")
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            var gunTransform = GetComponentInChildren<Transform>();
+            GetComponentInChildren<Transform>().localScale = new Vector3(gunTransform.localScale.x*-1, gunTransform.localScale.y, gunTransform.localScale.z);
+            moveSpeed *= -1f;
         }
     }
+
 }
