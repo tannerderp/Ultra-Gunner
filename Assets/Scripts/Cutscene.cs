a0fa5bc771e09297a3e615a277ef2b06bdccf586
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2020-01-18 12:24:40 -0700

    cutscene stuff
    Stuff i did so this would work on webgl, I did this stuff in november but didn't get around to uploading it til now

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -8,20 +8,29 @@ public class Cutscene : MonoBehaviour
 {
     public double time;
     public double currentTime;
+
+    private int wait = 0;
     // Use this for initialization
     void Start()
     {
-        time = gameObject.GetComponent<VideoPlayer>().clip.length;
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        currentTime = gameObject.GetComponent<VideoPlayer>().time;
-        if (currentTime > time-0.7)
+        wait++;
+        if(wait == 80)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            time = GetComponent<VideoPlayer>().frameCount/30 + 0.7;
+            Debug.Log(time);
+        }
+        if (wait > 80)
+        {
+            currentTime = GetComponent<VideoPlayer>().time;
+            if (currentTime > time - 0.7)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
     }
 }
